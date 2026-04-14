55b2464ea53e8bb2681397e86b8e0825d5ee1008
Reginaldo Sousa <reginaldo.rms@gmail.com>
2026-04-14 09:53:06 -0300

    feat: add Cloudflare API token secret and update ClusterIssuer to use DNS-01 challenge

--- cert-manager.tf
+++ cert-manager.tf
@@ -18,6 +18,26 @@ resource "helm_release" "cert-manager" {
   ]
 }
 
+# Cria o segredo para o token da API da Cloudflare no namespace do cert-manager.
+# Este segredo é necessário para que o Cert-Manager possa autenticar e gerenciar os registros DNS no Cloudflare, 
+# especialmente para o desafio DNS-01 usado na validação de certificados com o Let's Encrypt.
+resource "kubernetes_secret_v1" "cloudflare-api-token-cm" {
+  metadata {
+    name      = "cloudflare-api-token"
+    namespace = "cert-manager"
+  }
+
+  data = {
+    apiKey = var.cloudflare_api_token
+  }
+}
+
+# Cria um recurso Kubernetes do tipo ClusterIssuer para o Cert-Manager, 
+# configurado para usar o ACME com o Let's Encrypt.
+# O ClusterIssuer define como o Cert-Manager deve obter e renovar certificados TLS.
+# Neste caso, estamos configurando o ClusterIssuer para usar o desafio DNS-01 com o provedor Cloudflare, 
+# permitindo que o Cert-Manager gerencie os registros DNS necessários para validar a propriedade do domínio 
+# e obter os certificados TLS de forma automatizada.
 resource "kubernetes_manifest" "cluster-issuer" {
   manifest = {
     apiVersion = "cert-manager.io/v1"
@@ -34,9 +54,12 @@ resource "kubernetes_manifest" "cluster-issuer" {
         }
         solvers = [
           {
-            http01 = {
-              ingress = {
-                class = kubernetes_ingress_class_v1.ingress-class.metadata[0].name
+            dns01 = {
+              cloudflare = {
+                apiTokenSecretRef = {
+                  name = "cloudflare-api-token"
+                  key  = "apiKey"
+                }
               }
             }
           }
